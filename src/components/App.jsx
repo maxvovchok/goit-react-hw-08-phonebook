a93531628b5ce718273c9fd50477bd1310d2d915
MaksimVov <vovchokmakcim@gmail.com>
2023-07-21 19:48:48 +0300

    react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,62 @@
-// import { ContactForm } from './ContactForm';
-// import { ContactList } from './ContactList';
-// import { Filter } from './Filter';
-// import { nanoid } from 'nanoid';
-// import { useState, useEffect, useRef } from 'react';
+import { ContactForm } from './ContactForm';
+import { ContactList } from './ContactList';
+import { Filter } from './Filter';
+import { nanoid } from 'nanoid';
+import { useState, useEffect, useRef } from 'react';
 
-// export const App = () => {
-//   const [contacts, setContacts] = useState([]);
-//   const [filter, setFilter] = useState('');
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-//   const prevStateRef = useRef(null);
+  const prevStateRef = useRef(null);
 
-//   useEffect(() => {
-//     const localStorageContacts = JSON.parse(localStorage.getItem('contacts'));
-//     if (localStorageContacts) {
-//       setContacts(localStorageContacts);
-//     }
-//   }, []);
+  useEffect(() => {
+    const localStorageContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (localStorageContacts) {
+      setContacts(localStorageContacts);
+    }
+  }, []);
 
-//   const addContact = (name, number) => {
-//     const existingContacts = contacts.some(
-//       contact =>
-//         contact.name.toLowerCase() === name.toLowerCase() ||
-//         contact.number === number
-//     );
+  const addContact = (name, number) => {
+    const existingContacts = contacts.some(
+      contact =>
+        contact.name.toLowerCase() === name.toLowerCase() ||
+        contact.number === number
+    );
 
-//     if (existingContacts) {
-//       return alert('Rosie Simpson is already in contacts.');
-//     }
+    if (existingContacts) {
+      return alert('Rosie Simpson is already in contacts.');
+    }
 
-//     const newContact = { id: nanoid(), name, number };
+    const newContact = { id: nanoid(), name, number };
 
-//     setContacts([...contacts, newContact]);
+    setContacts([...contacts, newContact]);
 
-//     localStorage.setItem('contacts', JSON.stringify([...contacts, newContact]));
-//   };
+    localStorage.setItem('contacts', JSON.stringify([...contacts, newContact]));
+  };
 
-//   const handleFilterChange = filter => {
-//     setFilter(filter);
-//   };
+  const handleFilterChange = filter => {
+    setFilter(filter);
+  };
 
-//   const deleteContact = id => {
-//     localStorage.setItem(
-//       'contacts',
-//       JSON.stringify(contacts.filter(contact => contact.id !== id))
-//     );
-//     setContacts(contacts.filter(contact => contact.id !== id));
-//   };
+  const deleteContact = id => {
+    localStorage.setItem(
+      'contacts',
+      JSON.stringify(contacts.filter(contact => contact.id !== id))
+    );
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
 
-//   const filteredContacts = contacts.filter(contact =>
-//     contact.name.toLowerCase().includes(filter.toLowerCase())
-//   );
-//   return (
-//     <div>
-//       <h1>Phonebook</h1>
-//       <ContactForm addContact={addContact} />
-//       <h2>Contacts</h2>
-//       <Filter filter={filter} onFilterChange={handleFilterChange} />
-//       <ContactList contacts={filteredContacts} onDelete={deleteContact} />
-//     </div>
-//   );
-// };
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} onFilterChange={handleFilterChange} />
+      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
+    </div>
+  );
+};
