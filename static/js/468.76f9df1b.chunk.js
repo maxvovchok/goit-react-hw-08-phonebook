75d0db13baee4d9ca6742dfcde221723a899e343
maxvovchok <121551172+maxvovchok@users.noreply.github.com>
2023-08-26 10:41:18 +0000

    Deploying to gh-pages from @ maxvovchok/goit-react-hw-08-phonebook@a6aedcad30412dc4a72d060b74aba8e1adf4860b 🚀

---
+++ static/js/468.76f9df1b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[468],{7468:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"Home"})}}}]);
+//# sourceMappingURL=468.76f9df1b.chunk.js.map
